1d07d89756155d6a04351904a36cc1f336e49b5a
Stanislav <77130336+standbit@users.noreply.github.com>
2022-10-13 08:51:14 +0300

    Update functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -2,5 +2,5 @@ from selenium import webdriver
 
 #test that Django page opens
 browser = webdriver.Chrome("/mnt/c/Users/stani/Downloads/chromedriver_win32101022/chromedriver.exe")
-browser.get("http://localhost:8000")
-assert "Django" in browser.title
+browser.get("http://127.0.0.1:8000")
+assert "Congratulations" in browser.title
