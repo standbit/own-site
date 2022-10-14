5c6971060b1d67be98807abc870d06e27d78945a
Stanislav <77130336+standbit@users.noreply.github.com>
2022-10-14 21:50:42 +0300

    Create new model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    full_text = models.TextField()
+    summary = models.CharField(max_length=255)
+    category = models.CharField(max_length=50)
+    pubdate = models.DateTimeField()
+    # slug = ... # todo
+    # is_published = models.BooleanField() # todo
+
